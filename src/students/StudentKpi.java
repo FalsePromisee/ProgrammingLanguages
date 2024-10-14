110ba5d1a856f76cb74102f834743d4ff3a080dd
Павел Галковский <pasha.galkovsky22@gmail.com>
2024-10-14 23:48:44 +0300

    HomeWork7

---
+++ src/students/StudentKpi.java
@@ -0,0 +1,14 @@
+package students;
+
+public class StudentKpi extends StudentMain {
+
+
+    private int examMark;
+
+    public StudentKpi(int age, String name, String email, int examMark) {
+        super(age, name, email);
+        this.examMark = examMark;
+    }
+
+
+}
